ded6200c165f583275f40442de6c1becd5c53c8a
Paul Ambrose <pablo_rose34@hotmail.com>
2023-03-11 13:21:22 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-import * as dealerAndBlinds from "./javascriptCode/1_selectDealerAndBlinds.js";
+"use strict"
 
-//Create players
-let test = dealerAndBlinds.randomNumber(5);
+import * as gameSetupClass from "./javascriptCode/1_gameSetupClass.js";
+import * as playerClass from "./javascriptCode/2_playerClass.js";
+
+/*
+let testRandom = dealerAndBlinds.randomNumber(5);
+console.log(testRandom);
+let test = new playerClass.Player("paul", 500);
 console.log(test);
+*/
+
+//Initial set up of game
+let numberOfPlayers = 5;
+let startingChips = 500;
+
+//Initial set up of round
+let game = new gameSetupClass.game;
+game.selectDealer(numberOfPlayers);
+game.selectSmallBlind(game.dealer);
+game.selectBigBlind(game.smallBlind);
+
+console.log(game);
+console.log(game.dealer);
+console.log(game.smallBlind);
+console.log(game.bigBlind);
