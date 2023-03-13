b77d0084bef6959c21b3e44912ce990389e3ffde
Paul Ambrose <pablo_rose34@hotmail.com>
2023-03-13 20:46:57 +0000

    Add files via upload

---
+++ javascriptCode/3_cardDeckClass.js
@@ -0,0 +1,38 @@
+"use strict"
+
+class CardDeck {
+  constructor() {
+    this.cards = [];
+    this.suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    this.values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    for (let suit of this.suits) {
+      for (let value of this.values) {
+        this.cards.push(`${value} of ${suit}`);
+      }
+    }
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  deal(numCards) {
+    if (numCards > this.cards.length) {
+      throw new Error('Not enough cards in deck');
+    }
+
+    const cardsDealt = [];
+
+    for (let i = 0; i < numCards; i++) {
+      cardsDealt.push(this.cards.pop());
+    }
+
+    return cardsDealt;
+  }
+}
+
+export { CardDeck };
