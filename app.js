98bdfb499b8d3ac0004694b825e0eeb95232531e
Paul Ambrose <pablo_rose34@hotmail.com>
2023-03-28 20:54:12 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -6,15 +6,29 @@ import * as cardDeckClass from "./javascriptCode/3_cardDeckClass.js";
 //Initial set up of game
 let numberOfPlayers = 6;
 let game = new gameSetupClass.game;
-
 let testRandomRange = []
+let errorCount = 0
 
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < 1000; i++){
+
       let testDealer = game.selectDealer();
-      if (testDealer > (numberOfPlayers - 1)){
-          testRandomRange.push(testDealer);
+      if (testDealer > (numberOfPlayers) || testDealer < 0){
+          testRandomRange.push("ERROR");
       }else{
           testRandomRange.push(testDealer);
       }
-    }
-    console.log(testRandomRange);
+    };
+
+for(let i = 0; i < testRandomRange.length; ++i){
+
+    if(testRandomRange[i] == "ERROR"){
+      errorCount++;
+    };
+}
+
+if(errorCount == 0){
+  console.log("Test Passed")
+}
+if(errorCount != 0){
+  console.log("Test Failed")
+}
